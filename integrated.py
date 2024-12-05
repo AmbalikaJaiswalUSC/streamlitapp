5c27b978efcb1624ee5913374898d66dd00f6748
AmbalikaJaiswalUSC <ambalika@usc.edu>
2024-12-05 08:50:46 -0800

    Update integrated.py

--- integrated.py
+++ integrated.py
@@ -295,7 +295,7 @@ elif selected_section == "Best Place to Live":
         return pd.read_csv(file_path, encoding='ISO-8859-1')
 
     st.markdown(
-        "<h2 style='font-size: 28px; font-weight: bold;'>Fitness Centers and Grocery Shops across Los Angelese</h2>",
+        "<h2 style='font-size: 28px; font-weight: bold;'>Fitness Centers and Grocery Shops across Los Angeles</h2>",
         unsafe_allow_html=True
     )
 
